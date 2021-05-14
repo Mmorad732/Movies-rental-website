5f53faa5881183b7489554b23deb17ff25f8d662
Ahmed Mohab <Ahmed.Mohab@emc.com>
2021-05-14 14:23:39 +0200

    Clean the code from unwanted comments

--- app/controllers/sessions_controller.rb
+++ app/controllers/sessions_controller.rb
@@ -10,18 +10,16 @@ class SessionsController < ApplicationController
     
   	user = User.find_by(email: params[:session][:email].downcase)
     if user && user.authenticate(params[:session][:password])
-      # Log the user in and redirect to the user's show page.
+      
       log_in user
       redirect_to root_url
     else
-      #flash[:danger] = 'Invalid email/password combination' # Not quite right!
+      flash[:danger] = 'Invalid email/password combination' 
       render 'new'
     end
   end
 
   def destroy
-  #  redirect_back(fallback_location: root_path,data: { confirm: 'Are you sure?' })
-   # @popup = true
     log_out
    
   end
